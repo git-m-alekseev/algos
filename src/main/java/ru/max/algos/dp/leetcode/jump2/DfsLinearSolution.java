8774b17975a10ac4a37e37e98ad0964ceaccbb02
malekseev <git.m.alekseev@gmail.com>
2025-12-07 02:32:53 +0300

    dynamic programming: min jumps (linear)

--- src/main/java/ru/max/algos/dp/leetcode/jump2/DfsLinearSolution.java
+++ src/main/java/ru/max/algos/dp/leetcode/jump2/DfsLinearSolution.java
@@ -4,6 +4,18 @@ public class DfsLinearSolution implements Solution {
 
     @Override
     public int minJumps(int[] nums) {
-        return 0;
+        int curEnd = 0;
+        int farthest = 0;
+        int jumps = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (curEnd == i) {
+                jumps++;
+                curEnd = farthest;
+            }
+        }
+
+        return jumps;
     }
 }
